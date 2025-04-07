df231e12bbc0029833e87fa8383e9607807ba3f6
Cho2unseo <escces00@gmail.com>
2025-04-07 17:12:39 +0900

    feat: 회원가입

---
+++ src/pages/SignupPage.vue
@@ -0,0 +1,149 @@
+<template>
+  <div id="app">
+    <div id="signup">
+      <section>
+        <div id="signup__header">
+          <h1>정보를 설정해주세요.</h1>
+        </div>
+        <div style="height: 64px"></div>
+        <div id="signup__form">
+          <p>아이디</p>
+          <input type="text" placeholder="아이디를 입력해주세요." />
+          <div id="idbox"></div>
+          <button id="checkid">중복 확인</button>
+          <p>비밀번호</p>
+          <input
+            type="password"
+            v-model="password"
+            placeholder="비밀번호를 입력해주세요."
+          />
+          <p>비밀번호 확인</p>
+          <input
+            type="password"
+            v-model="checkpassword"
+            placeholder="비밀번호를 확인해주세요."
+          />
+          <p v-if="password !== checkpassword" style="color: #ff738f">
+            비밀번호가 다릅니다.
+          </p>
+          <p>생년월일</p>
+          <select v-model="year">
+            <option disabled>년</option>
+            <option>2020</option>
+          </select>
+          &nbsp;
+          <select v-model="month">
+            <option disabled>월</option>
+            <option>1</option>
+          </select>
+          &nbsp;
+          <select v-model="day">
+            <option disabled>일</option>
+            <option>1</option>
+          </select>
+        </div>
+      </section>
+      <section>
+        <div style="height: 104px"></div>
+        <div id="terms">
+          <input type="checkbox" /> 약관 1
+          <br />
+          <input type="checkbox" /> 약관 2
+        </div>
+      </section>
+      <button id="submit-btn">회원가입</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const password = ref('');
+const checkpassword = ref('');
+const year = ref('년');
+const month = ref('월');
+const day = ref('일');
+</script>
+
+<style scoped>
+#app {
+  width: 100%;
+  height: 100vh;
+  position: relative;
+  font-family: 'Noto Sans KR', sans-serif;
+}
+
+#signup {
+  position: absolute;
+  width: 80%;
+  height: 80%;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding: 48px;
+  padding-bottom: 128px;
+  border-radius: 10px;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  background-color: #fcfcfc;
+  display: flex;
+}
+
+#signup > section {
+  flex: 1;
+}
+
+#signup__header h1 {
+  display: inline;
+  font-size: 32px;
+  font-style: normal;
+  font-weight: 700;
+}
+
+#idbox {
+  width: 16px;
+  display: inline-block;
+}
+
+#signup__form {
+  margin: 0 32px;
+}
+
+#signup__form p {
+  font-weight: 700;
+  margin: 8px 0;
+}
+
+input {
+  margin: 8px 0;
+  border: none;
+  text-decoration: underline rgb(216, 216, 216);
+  background-color: #fcfcfc;
+}
+
+#checkid {
+  border-radius: 10px;
+  padding: 16px;
+  border: none;
+  background-color: #cdcdcd;
+  color: white;
+}
+
+select {
+  margin: 8px 0;
+}
+
+#submit-btn {
+  position: absolute;
+  bottom: 80px;
+  left: 50%;
+  transform: translateX(-50%);
+  border: none;
+  border-radius: 10px;
+  width: 300px;
+  height: 50px;
+  background-color: rgba(128, 155, 255, 0.75);
+  color: white;
+  font-weight: 700;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,45 @@
-import { createRouter, createWebHistory } from "vue-router";
-import LoginPage from "@/pages/LoginPage.vue";
-import MainPage from "@/pages/MainPage.vue";
-import AccountList from "@/pages/AccountList.vue";
-import AddList from "@/pages/AddList.vue";
+import { createRouter, createWebHistory } from 'vue-router';
+import LoginPage from '@/pages/LoginPage.vue';
+import MainPage from '@/pages/MainPage.vue';
+import AccountList from '@/pages/AccountList.vue';
+import AddList from '@/pages/AddList.vue';
+import SignupPage from '@/pages/SignupPage.vue';
+import Mypage from '@/pages/Mypage.vue';
 
 const router = createRouter({
-	history: createWebHistory(import.meta.env.BASE_URL),
-	routes: [
-		{
-			path: "/",
-			name: "login",
-			component: LoginPage,
-		},
-		{
-			path: "/main",
-			name: "main",
-			component: MainPage,
-		},
-		{
-			path: "/account",
-			name: "account",
-			component: AccountList,
-		},
-		{
-			path: "/account/:id",
-			name: "account/id",
-			component: AddList,
-		},
-	],
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: LoginPage,
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: SignupPage,
+    },
+    {
+      path: '/main',
+      name: 'main',
+      component: MainPage,
+    },
+    {
+      path: '/account',
+      name: 'account',
+      component: AccountList,
+    },
+    {
+      path: '/account/:id',
+      name: 'account/id',
+      component: AddList,
+    },
+    {
+      path: '/mypage',
+      name: 'mypage',
+      component: Mypage,
+    },
+  ],
 });
 
 export default router;
