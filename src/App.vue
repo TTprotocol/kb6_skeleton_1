722d4c4b7ece5bccc4abe8d6df048be04deb2af4
Cho2unseo <escces00@gmail.com>
2025-04-07 11:47:23 +0900

    feat: 로그인 페이지 일부

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
-	<div>
-		<Header />
-		<router-view></router-view>
-		<Footer />
-	</div>
+  <div>
+    <Header />
+    <router-view></router-view>
+    <Footer />
+  </div>
 </template>
 
 <script setup>
-import Header from "./component/Header.vue";
-import Footer from "./component/Footer.vue";
+import Header from './component/Header.vue';
+import Footer from './component/Footer.vue';
 </script>
 
-<style scoped></style>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap');
+</style>
--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,34 +1,70 @@
 <template>
-	<div id="app">
-		<div id="login">
-			<div id="login__header">
-				<h1>반갑습니다.</h1>
-			</div>
-			<div id="login__body">
-				<h1>로그인하기</h1>
-				<button>로그인하기</button>
-			</div>
-		</div>
-	</div>
+  <div id="app">
+    <div id="login">
+      <div id="login__header">
+        <h1>반갑습니다.</h1>
+        <br /><br />
+        <h1>간편하게&nbsp;</h1>
+        <h1 id="login__h1">로그인&nbsp;</h1>
+        <h1>해보세요!</h1>
+      </div>
+      <div id="login__tools"></div>
+      <br />
+      <div id="login__body">
+        <button id="id__login">아이디로 로그인하기</button>
+        <br /><br />
+        <p>아직 회원이 아닌가요?</p>
+        <button id="sign__up">회원가입하기</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup></script>
 
 <style scoped>
 #app {
-	width: 100%;
-	height: 100vh;
-	position: relative;
+  width: 100%;
+  height: 100vh;
+  position: relative;
+  font-family: 'Noto Sans KR', sans-serif;
 }
 
 #login {
-	position: absolute;
-	width: 80%;
-	height: 80%;
-	border: 1px solid red;
-	top: 50%;
-	left: 50%;
-	transform: translate(-50%, -50%);
-	padding: 30px;
+  position: absolute;
+  width: 80%;
+  height: 80%;
+  border: 1px solid red;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding: 30px;
+}
+
+#login__header h1 {
+  display: inline;
+}
+
+#login__body p {
+  display: inline;
+}
+
+#login__h1 {
+  color: #ffff3c;
+  text-shadow: 2px 2px rgb(203, 203, 203);
+}
+
+#id__login {
+  background-color: #ffff3c;
+  border-radius: 10px;
+  padding: 10px;
+  border: none;
+}
+
+#sign__up {
+  border: none;
+  font-weight: bold;
+  text-decoration: underline;
+  background-color: white;
 }
 </style>
