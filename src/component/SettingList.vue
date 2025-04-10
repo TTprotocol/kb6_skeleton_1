f40daadacae33f33b721c3e2bc44c098bf67a79a
Cho2unseo <escces00@gmail.com>
2025-04-10 11:42:29 +0900

    feat: 사용자 정보 수정 기능 구현

--- src/component/SettingList.vue
+++ src/component/SettingList.vue
@@ -9,9 +9,15 @@
     <teleport to="#question_modal">
       <QuestionModal v-if="isModal2" @close="isModal2 = false" />
     </teleport>
-    <button id="menus__btn">설정</button>
-    <button id="menus__btn" @click="">로그아웃</button>
-
+    <button id="menus__btn" @click="changeModal3">예산 설정</button>
+    <teleport to="#question_modal">
+      <BudgetModal
+        v-if="isModal3"
+        @close="isModal3 = false"
+        @setting-budget="settingBudget"
+      />
+    </teleport>
+    <button id="menus__btn" @click="logout">로그아웃</button>
     <button id="menus__btn" @click="changeModal5">탈퇴하기</button>
     <teleport to="#delete_account_modal">
       <DeleteAccountModal v-if="isModal5" @close="isModal5 = false" />
@@ -21,13 +27,24 @@
 
 <script setup>
 import { ref } from 'vue';
+import { loginStore } from '@/stores/LoginStore';
 import NoticeModal from './NoticeModal.vue';
 import QuestionModal from './QuestionModal.vue';
 import DeleteAccountModal from './DeleteAccountModal.vue';
+import { useRouter } from 'vue-router';
+import BudgetModal from './BudgetModal.vue';
 
 const isModal = ref(false);
 const isModal2 = ref(false);
+const isModal3 = ref(false);
 const isModal5 = ref(false);
+const store = loginStore();
+const router = useRouter();
+
+const logout = async () => {
+  await store.logoutData();
+  router.push('/');
+};
 
 const changeModal = () => {
   isModal.value = true;
@@ -37,8 +54,16 @@ const changeModal2 = () => {
   isModal2.value = true;
 };
 
+const changeModal3 = () => {
+  isModal3.value = true;
+};
+
 const changeModal5 = () => {
   isModal5.value = true;
+};
+
+const settingBudget = async (money) => {
+  await store.settingBudget(money);
 };
 </script>
 
--- src/stores/LoginStore.js
+++ src/stores/LoginStore.js
@@ -50,26 +50,101 @@ export const loginStore = defineStore('login', () => {
     }
   };
 
-  const settingUser = async (password, newpassword, birth, gender) => {
+  const logoutData = async () => {
     try {
       const response = await axios.get(BASEURI);
       if (response.status === 200) {
         const userList = response.data;
+        const matchUser = userList.find((user) => user.login === true);
+        if (matchUser) {
+          state.email = matchUser.email;
+          state.name = matchUser.name;
+          state.birth = matchUser.birth;
+          state.gender = matchUser.gender;
+          state.budget = matchUser.budget;
+          state.login = true;
+          console.log(matchUser);
+          await axios.put(`${BASEURI}/${matchUser.id}`, {
+            ...matchUser,
+            login: false,
+          });
+
+          console.log('로그아웃 성공');
+          return true;
+        }
+      } else {
+        console.log('데이터 조회 실패');
+      }
+    } catch (e) {
+      console.log('데이터 조회 에러 발생');
+    }
+  };
+
+  const settingBudget = async (newbudget) => {
+    try {
+      const response = await axios.get(BASEURI);
+      if (response.status === 200) {
+        const userList = response.data;
+        const matchUser = userList.find((user) => user.login === true);
+        if (matchUser) {
+          state.email = matchUser.email;
+          state.name = matchUser.name;
+          state.birth = matchUser.birth;
+          state.gender = matchUser.gender;
+          state.budget = matchUser.budget;
+          state.login = true;
+          console.log(matchUser);
+          await axios.put(`${BASEURI}/${matchUser.id}`, {
+            ...matchUser,
+            budget: newbudget,
+          });
+          console.log('예산 업데이트 성공');
+          return true;
+        }
+      } else {
+        console.log('데이터 조회 실패');
+      }
+    } catch (e) {
+      console.log('데이터 조회 에러 발생');
+    }
+  };
+
+  const settingUser = async (
+    password,
+    newpassword,
+    year,
+    month,
+    day,
+    gender
+  ) => {
+    try {
+      const response = await axios.get(BASEURI);
+      if (response.status === 200 || response.status === 201) {
+        const userList = response.data;
         const matchUser = userList.find(
           (user) => user.email === state.email && user.password === password
         );
+        console.log('check + ', matchUser);
+
         if (!matchUser) {
           console.log('현재 비밀번호가 다릅니다.');
           return false;
         }
         const updatedUser = {
+          email: matchUser.email,
+          name: matchUser.name,
+          budget: matchUser.budget,
           password: newpassword || matchUser.password,
-          birth: birth || matchUser.birth,
-          gender: gender || matchUser.birth,
+          birth: `${year}-${month}-${day}` || matchUser.birth,
+          gender: gender || matchUser.gender,
+          login: true,
         };
         await axios.put(`${BASEURI}/${matchUser.id}`, updatedUser);
-        state.birth = updatedUser.birth;
+        state.year = updatedUser.year;
+        state.month = updatedUser.month;
+        state.day = updatedUser.day;
         state.gender = updatedUser.gender;
+        state.birth = updatedUser.birth;
         if (newpassword) console.log('비밀번호 변경 완료');
         console.log('유저 정보 업데이트 완료');
         return true;
@@ -161,6 +236,8 @@ export const loginStore = defineStore('login', () => {
     getMine,
     checkId,
     createData,
+    logoutData,
+    settingBudget,
     myEmail,
     myName,
     myBirth,
