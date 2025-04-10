e8c43fee0fa0207ca160e09213d9303af55c4350
TTprotocol <wnsdud7941@gmail.com>
2025-04-10 17:44:12 +0900

    # 필터 기능 추가 완료

--- src/component/TransactionTable.vue
+++ src/component/TransactionTable.vue
@@ -1,5 +1,5 @@
 <!-- src/components/TransactionTable.vue -->
-<template>
+<!-- <template>
 	<div id="table" class="table-responsive p-2" style="height: 100%">
 		<table class="table table-hover align-middle table-sm align-middle">
 			<thead class="table-light">
@@ -13,6 +13,15 @@
 					<th scope="col" class="text-end">금액</th>
 					<th scope="col">메모</th>
 					<th scope="col">삭제</th>
+					<th class="text-end">
+						<button
+							class="btn btn-light border"
+							@click="$emit('open-filter')"
+							title="필터 열기"
+						>
+							<i class="bi bi-funnel-fill"></i>
+						</button>
+					</th>
 				</tr>
 			</thead>
 
@@ -71,9 +80,9 @@
 			</button>
 		</div>
 	</div>
-</template>
+</template> -->
 
-<script setup>
+<!-- <script setup>
 import { getAccountListStore } from "@/stores/GetAccountListStore";
 import { computed, watch } from "vue";
 import { useRouter } from "vue-router";
@@ -94,7 +103,7 @@ const props = defineProps({
 // 	return store.pageList.sort((a, b) => (a.date > b.date ? -1 : 1));
 // });
 
-const emit = defineEmits(["update:checkedItems"]);
+const emit = defineEmits(["update:checkedItems", "open-filter"]);
 
 const checkedItems = defineModel("checkedItems");
 
@@ -107,6 +116,107 @@ const toggleAll = (event) => {
 		checkedItems.value = [];
 	}
 };
-</script>
+</script> -->
 
-<style scoped></style>
+<!-- <style scoped></style> -->
+<template>
+	<!-- <div class="table-wrapper"> -->
+	<div class="table-responsive rounded-2">
+		<table class="table table-hover align-middle mb-0">
+			<thead class="table-light">
+				<tr>
+					<th scope="col">
+						<input type="checkbox" @change="toggleAll" />
+					</th>
+					<th scope="col">날짜</th>
+					<th scope="col">카테고리</th>
+					<th scope="col">내용</th>
+					<th scope="col" class="text-end">금액</th>
+					<th scope="col">메모</th>
+					<th scope="col">삭제</th>
+					<th class="text-end">
+						<button
+							class="btn btn-light border"
+							@click="$emit('open-filter')"
+							title="필터 열기"
+						>
+							<i class="bi bi-funnel-fill"></i>
+						</button>
+					</th>
+				</tr>
+			</thead>
+
+			<tbody>
+				<tr v-for="(item, index) in data" :key="index">
+					<td>
+						<input type="checkbox" :value="item" v-model="checkedItems" />
+					</td>
+					<td>{{ item.date }}</td>
+					<td>{{ item.category }}</td>
+					<td>{{ item.description }}</td>
+					<td
+						:class="[
+							'text-end',
+							item.type === 1 ? 'text-primary' : 'text-danger',
+						]"
+					>
+						{{ item.amount.toLocaleString() }}
+					</td>
+					<td>{{ item.memo }}</td>
+					<td>
+						<button
+							class="btn btn-danger fw-bold"
+							@click.stop="deleteData(item.id)"
+						>
+							삭제
+						</button>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+		<div class="d-flex justify-content-around align-items-center mt-3">
+			<button
+				class="btn btn-outline-primary"
+				@click="prevPage(currentTab)"
+				:disabled="store.currentPage === 1"
+			>
+				이전
+			</button>
+			<span>Page {{ store.currentPage }}</span>
+			<button class="btn btn-outline-primary" @click="nextPage(currentTab)">
+				다음
+			</button>
+		</div>
+	</div>
+	<!-- </div> -->
+</template>
+
+<script setup>
+import { getAccountListStore } from "@/stores/GetAccountListStore";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const store = getAccountListStore();
+const { fetchAllList, fetchPageList, nextPage, prevPage, deleteData } = store;
+
+fetchAllList();
+fetchPageList();
+
+const props = defineProps({
+	data: Array,
+	currentTab: String,
+});
+
+const emit = defineEmits(["update:checkedItems", "open-filter"]);
+
+const checkedItems = defineModel("checkedItems");
+
+// 전체 선택
+const toggleAll = (event) => {
+	if (event.target.checked) {
+		checkedItems.value = [...props.data];
+	} else {
+		checkedItems.value = [];
+	}
+};
+</script>
